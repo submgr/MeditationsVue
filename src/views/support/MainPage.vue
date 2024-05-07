c3fc08f6f8a76c77bc265d3ce1ad11daef641063
Aram Virabyan <googlaram200@gmail.com>
2024-05-07 21:31:52 +0300

    Special support channel for VK users

--- src/views/support/MainPage.vue
+++ src/views/support/MainPage.vue
@@ -10,7 +10,8 @@
 
         <h1 style="margin-left: 1.1rem; margin-top: 2.4rem; font-weight: 700; font-size: 34px;">Поддержка</h1>
 
-        <div style="margin-left: 1.1rem; margin-right: 2vw;">
+        <div v-if="appSource == 'general'">
+          <div style="margin-left: 1.1rem; margin-right: 2vw;">
             <ion-label><br/>Если у вас возникли вопросы, вы можете задать их нам удобным для вас способом, например, в онлайн-чате или по электронной почте.<br/><br/>
                 Иногда никого из нашей команды нет в сети, поэтому, если вы не получили ответа в чате, напишите нам на электронную почту, и мы ответим, как только сможем.</ion-label>
         </div>
@@ -44,6 +45,32 @@
               </ion-col>
             </ion-row>
           </ion-grid>
+        </div>
+
+        <div v-if="appSource == 'vk'">
+          <div style="margin-left: 1.1rem; margin-right: 2vw;">
+            <ion-label><br/>Если у вас возникли вопросы, напишите нам в чате во ВКонтакте. Мы будем рады вам ответить.</ion-label>
+        </div>
+        
+        <ion-grid>
+
+            
+            <ion-row>
+              <ion-col style="margin-right: -3vw; margin-left: -2vw;" @click="supportOption_vk">
+                <ion-card class="ion-activatable ripple-parent rectangle">
+                    <ion-ripple-effect></ion-ripple-effect>
+                    <ion-card-header>
+                      <ion-card-title><ion-icon style="font-size: 5vh; margin-top: 1vh;" :icon="chatbubblesOutline"></ion-icon></ion-card-title>
+                    </ion-card-header>
+                    <ion-card-content>
+                        Чат ВКонтакте
+                      </ion-card-content>
+                  </ion-card>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </div>
+
         
       </ion-content>
     </ion-page>
@@ -63,6 +90,23 @@
     components: { IonContent, IonPage, IonGrid, IonRow, IonCol, IonCard, IonLabel, IonCardHeader, IonCardTitle, IonCardContent, IonRippleEffect, NavbarController  },
     mounted(){
         //
+        var isVKMiniApps = false;
+        try {
+            // Try to get the value from localStorage
+            isVKMiniApps = localStorage.getItem('isVKMiniApps') === 'true';
+        } catch (e) {
+            // If an error occurs (e.g., localStorage is not available), keep isVKMiniApps as false
+            console.log('Failed to access localStorage. Defaulting isVKMiniApps to false.');
+        }
+
+        if(isVKMiniApps){
+            this.appSource = "vk";
+        }
+    },
+    data() {
+      return {
+        appSource: "general"
+      }
     },
     methods: {
         supportOption_chat(){
@@ -70,6 +114,9 @@
         },
         supportOption_email(){
             (window as any).location = "mailto:support@deqstudio.com";
+        },
+        supportOption_vk(){
+          window.open('https://vk.me/urmeditation');
         }
     },
     setup(){
