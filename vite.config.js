184a2c3a0e86a49d1dac87026b89578d44784b47
Aram Virabyan <googlaram200@gmail.com>
2024-05-11 15:32:29 +0300

    New image compression engine just got implemented

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import svgLoader from 'vite-svg-loader'
 import WindiCSS from 'vite-plugin-windicss'
+
 import { resolve } from 'path'
 
 import { dynamicImport } from 'vite-plugin-dynamic-import'
@@ -15,7 +17,38 @@ export default defineConfig({
   assetsInclude: ['**/*.png'],
   plugins: [vue(), svgLoader({
     defaultImport: 'url',
-  }), WindiCSS(), ],
+  }), WindiCSS(),
+
+  ViteImageOptimizer({
+    /* pass your config */
+    jpeg: {
+      quality: 80,
+      force: false
+    },
+    png: {
+      quality: 83,
+      force: false,
+      compressionLevel: 9
+    },
+    jpg: {
+      // https://sharp.pixelplumbing.com/api-output#jpeg
+      quality: 80,
+    },
+    tiff: {
+      // https://sharp.pixelplumbing.com/api-output#tiff
+      quality: 80,
+    },
+    webp: {
+      // https://sharp.pixelplumbing.com/api-output#webp
+      lossless: true,
+    },
+    avif: {
+      // https://sharp.pixelplumbing.com/api-output#avif
+      lossless: true,
+    },
+    logStats: true
+  }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(pathSegments, './src'),
