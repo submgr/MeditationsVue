52fc23994cf82cb78b711a8ee8e3a62a186e5718
Aram Virabyan <googlaram200@gmail.com>
2024-05-15 13:01:49 +0300

    Update

--- src/components/markdown/RenderContent.vue
+++ src/components/markdown/RenderContent.vue
@@ -15,7 +15,8 @@ export default defineComponent({
     name: 'ExploreContainer',
     props: {
         materialType: String,
-        codename: String
+        codename: String,
+        metatagsExist: Boolean
 
     },
     components: {
@@ -40,13 +41,16 @@ export default defineComponent({
         this.$http.get(globaldata.markdown_assets.hostname + "markdown/" + finalPathType + "/" + this.codename + ".md")
             .then(function (response) {
                 var received_content = response.data;
-                const metadata_tagmarker = ["<<metadata_title>>", "<</metadata_title>>"]
-                const metadata_tagplacement = [
-                    [received_content.indexOf(metadata_tagmarker[0]), received_content.indexOf(metadata_tagmarker[0]) + metadata_tagmarker[0].length],
-                    [received_content.indexOf(metadata_tagmarker[1]), received_content.indexOf(metadata_tagmarker[1]) + metadata_tagmarker[1].length],
-                ]
-                const extractedmeta_title = received_content.substring(metadata_tagplacement[0][1], metadata_tagplacement[1][0]);
-                received_content = received_content.slice(metadata_tagplacement[1][1], received_content.length);
+                if (parent_this.metatagsExist != false) {
+                    const metadata_tagmarker = ["<<metadata_title>>", "<</metadata_title>>"]
+                    const metadata_tagplacement = [
+                        [received_content.indexOf(metadata_tagmarker[0]), received_content.indexOf(metadata_tagmarker[0]) + metadata_tagmarker[0].length],
+                        [received_content.indexOf(metadata_tagmarker[1]), received_content.indexOf(metadata_tagmarker[1]) + metadata_tagmarker[1].length],
+                    ]
+                    const extractedmeta_title = received_content.substring(metadata_tagplacement[0][1], metadata_tagplacement[1][0]);
+                    received_content = received_content.slice(metadata_tagplacement[1][1], received_content.length);
+                }
+
                 parent_this.src = received_content;
             })
             .catch(function (error) {
