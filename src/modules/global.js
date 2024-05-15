df6889e7b0a6f18877459c9158afce12f383ab38
Aram Virabyan <googlaram200@gmail.com>
2024-05-15 12:45:06 +0300

    Updated CF-Pages address, now it points to deqstudio subdomain instead of *.pages.dev

--- src/modules/global.js
+++ src/modules/global.js
@@ -1,7 +1,7 @@
 var apiserver_hostname;
 
 var assetsbase_hostname = "https://xn--80aaafmfwb5a7d2bq4h.xn--p1ai/assets/";
-var markdown_assetsbase_hostname = "https://accessmeditation.pages.dev/"
+var markdown_assetsbase_hostname = "https://access.yourmeditation.deqstudio.com/"
 
 function isLocalHost(url) {
   return url.indexOf('localhost') !== -1 || url.indexOf('127.0.0.1') !== -1 || url.indexOf('192.') !== -1;
